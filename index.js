37ecd6a678cb719f810d89a6afcb0124db521776
pedropaulocg <pedropaulocostaguima@hotmail.com>
2023-12-11 14:18:59 -0300

    fixes

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const namesArray = [
   { nome: "Tio Paulo", took: false },
   { nome: "Paula", took: false },
   { nome: "Vovó Sina", took: false },
-  { nome: "Tia Mila", took: false }
+  { nome: "Tia Mila", took: false },
+  { nome: "Laura", took: false },
+  { nome: "Luisa", took: false }
 ];
 const participants = [
   { nome: "Pedro", took: false },
@@ -18,37 +20,59 @@ const participants = [
   { nome: "Tio Paulo", took: false },
   { nome: "Paula", took: false },
   { nome: "Vovó Sina", took: false },
-  { nome: "Tia Mila", took: false }
+  { nome: "Tia Mila", took: false },
+  { nome: "Laura", took: false },
+  { nome: "Luisa", took: false }
 ];
 
-function getRandomName() {
-  const randomIndex = Math.floor(Math.random() * namesArray.length);
+function getRandomName(user) {
+  const availableNames = namesArray.filter(item => !item.took && item.nome !== user);
+  if (availableNames.length === 0) {
+    return null;
+  }
+
+  const randomIndex = Math.floor(Math.random() * availableNames.length);
   return {
-    name: namesArray[randomIndex].nome,
-    index: randomIndex
+    name: availableNames[randomIndex].nome,
+    index: namesArray.findIndex(item => item.nome === availableNames[randomIndex].nome)
   };
 }
-const sortearNome = () => {
-  const { name, index } = getRandomName()
-  namesArray.splice(index, 1)
-  
-  return name
+
+const sortearNome = (user) => {
+  const result = getRandomName(user);
+
+  if (!result) {
+    return null;
+  }
+
+  const { name, index } = result;
+  namesArray[index].took = true;
+  return name;
 }
 
-  app.get("/sortear/:user", (req, res) => {
-  const {user} = req.params
-  const idx = participants.findIndex(item => user == item.nome)
-  if(participants[idx].took) {
-    return res.status(403).json("Voce ja tirou o seu!")
+app.get("/sortear/:user", (req, res) => {
+  const { user } = req.params;
+  const idx = participants.findIndex(item => user === item.nome);
+
+  if (idx === -1) {
+    return res.status(404).json("Usuário não encontrado");
   }
-  namesArray.splice(idx, 1)
-  const name = sortearNome()
-  participants[idx].took = true
-  namesArray.push({nome: user, took: false})
 
-  return res.status(200).json({name})
-})
+  if (participants[idx].took) {
+    return res.status(403).json("Você já tirou o seu!");
+  }
 
+  const name = sortearNome(user);
+
+  if (!name) {
+    
+    return res.status(403).json("Todos os nomes já foram sorteados");
+  }
+
+  participants[idx].took = true;
+
+  return res.status(200).json({ name });
+});
 const port = 3030
 
 app.listen(port, () => {
