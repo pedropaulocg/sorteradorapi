63402fb022986e6126a0c7c95b17fa26863dc3a2
pedropaulocg <pedropaulocostaguima@hotmail.com>
2023-12-12 11:12:32 -0300

    port

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get("/sortear/:user", (req, res) => {
 
   return res.status(200).json({ name });
 });
-const port = 443
+const port = 3030
 
 app.listen(port, () => {
   console.log('Server listen o ' + port)
